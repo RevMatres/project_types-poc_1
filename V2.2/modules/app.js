b791359ae287321184bc705b4f574300081acc0e
RevMatres <benediktheinen1@gmail.com>
2017-10-25 10:44:24 +0200

    added initial file setup for next Version

---
+++ V2.2/modules/app.js
@@ -0,0 +1,25 @@
+
+//
+//
+//    APP.JS
+//    this file interacts with the API specified in canvas.js
+//
+//
+
+// =============================================================================
+
+//
+//  Imports
+//
+
+import { Canvas } from "./canvas.js"
+
+// =============================================================================
+
+//
+//  Setup a few canvas elements
+//
+
+let canvas1 = new Canvas("c1", 500, 500, 100)
+let canvas2 = new Canvas("c2", 500, 500, 10)
+let canvas3 = new Canvas("c3", 500, 500, 1)
